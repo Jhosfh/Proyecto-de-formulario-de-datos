cc9b0936b2f0f11852dd2c78dee6dc753b97c8be
Jhosfh <145510138+Jhosfh@users.noreply.github.com>
2023-11-08 16:25:55 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$edad = $_POST['edad'];
+echo "<h2>Datos personales</h2>";
+echo "<p>Nombre: $nombre</p>";
+echo "<p>Apellido: $apellido</p>";
+echo "<p>Edad: $edad</p>";
+?>
